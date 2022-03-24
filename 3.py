a2dabf458267e46129e75a7e376d5b440ed6538b
Evgeniy <99466328+zhenya0914@users.noreply.github.com>
2022-03-25 05:57:31 +0600

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,6 @@
+import math
+n=float(input())
+a=float(input())
+r=a/(2*math.tan((math.pi)/n))
+area=n*a*r/2
+print (area)
---
+++ 4.py
@@ -0,0 +1,5 @@
+import math
+b=float(input())
+h=float(input())
+area = b*h
+print (area)
